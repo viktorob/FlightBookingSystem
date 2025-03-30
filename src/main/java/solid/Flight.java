c092c6f22b1fb8d73ed70ede0f201c35829de0a8
Viktor Bjarkason <vbjarkason@gmail.com>
2025-03-30 21:02:21 +0000

    Flight grunnur útfærður

--- src/main/java/solid/Flight.java
+++ src/main/java/solid/Flight.java
@@ -1,4 +1,5 @@
 package solid;
+import java.util.Date;
 
 public class Flight {
     private String flightNumber;
@@ -8,5 +9,28 @@ public class Flight {
     private String destAirport;
     private Date departureTime;
     private Date arrivalTime;
+    private int availableSeats;
+    private double price;
+    private float co2Emissions;
+
+    public Flight(String flightNumber, String airline, String aircraftModel, String srcAirport, String destAirport, Date departureTime, Date arrivalTime, int availableSeats, double price, float co2Emissions) {
+        this.flightNumber = flightNumber;
+        this.airline = airline;
+        this.aircraftModel = aircraftModel;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.availableSeats = availableSeats;
+        this.price = price;
+        this.co2Emissions = co2Emissions;
+    }
+
+    public Flight getFlightDetails() {
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return flightNumber + " from " + this.srcAirport + " to " + this.destAirport + " at " + departureTime;
+    }
 
 }
--- src/main/java/solid/FlightController.java
+++ src/main/java/solid/FlightController.java
@@ -1,5 +1,5 @@
 package solid;
 
 public class FlightController {
-
+    private Flight[] flights;
 }
